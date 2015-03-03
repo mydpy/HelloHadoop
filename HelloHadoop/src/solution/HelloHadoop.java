b542652d347de39f64363e1dba49d8016beafccd
Myles Baker <mylesdanielbaker@gmail.com>
2015-03-03 07:24:09 -0800

    Initial commit.

---
+++ HelloHadoop/src/solution/HelloHadoop.java
@@ -0,0 +1,40 @@
+package solution;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+
+public class HelloHadoop {
+
+  public static void main(String[] args) throws Exception {
+
+    if (args.length != 2) {
+      System.out.printf("Usage: AvgWordLength <input dir> <output dir>\n");
+      System.exit(-1);
+    }
+    
+    Configuration conf = new Configuration(); 
+	Job job = Job.getInstance(conf); 
+    job.setJarByClass(HelloHadoop.class);
+    job.setJobName("Hello Hadoop");
+
+    FileInputFormat.setInputPaths(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    job.setMapperClass(WordCountMapper.class);
+    job.setReducerClass(SumReducer.class);
+
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(IntWritable.class);
+
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(DoubleWritable.class);
+
+    boolean success = job.waitForCompletion(true);
+    System.exit(success ? 0 : 1);
+  }
+}
---
+++ HelloHadoop/src/solution/WordCountMapper.java
@@ -0,0 +1,25 @@
+package solution;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+  public static IntWritable one = new IntWritable(1); 
+  
+  @Override
+  public void map(LongWritable key, Text value, Context context)
+      throws IOException, InterruptedException {
+
+    String line = value.toString();
+
+    for (String word : line.split("\\W+")) {
+      if (word.length() > 0) {
+        context.write(new Text(word), one);
+      }
+    }
+  }
+}
